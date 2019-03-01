d3258c5128e3028680267eba443fe93e8fb9097f
GhiathMutlak <ghiath.mutlak@gmail.com>
2019-03-01 15:14:14 +0300

    Add listeners to the dialpad buttons.

--- app/src/main/java/com/digits/business/phoneauth/CountryData.java
+++ app/src/main/java/com/digits/business/phoneauth/CountryData.java
@@ -1,7 +1,7 @@
 package com.digits.business.phoneauth;
 
 class CountryData {
-    static final String[] countryNames = {"Australia", "Canada", "India", "Spain", "UK", "USA"};
+    static final String[] countryNames = {"Australia", "Canada", "India", "Spain", "UK", "USA", "Turkey", "Syria"};
 
-    static final String[] countryAreaCodes = {"+61", "+1", "+91", "+34", "+44", "+1"};
+    static final String[] countryAreaCodes = {"+61", "+1", "+91", "+34", "+44", "+1", "+90", "+963"};
 }
